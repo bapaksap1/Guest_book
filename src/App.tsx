8906bffd526ca1f80b411c63f3550d1fa96b137c
kaeltorus <mikhaeltorus@gmail.com>
2023-02-16 23:11:25 +0700

    login gan

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Home from './GuestBook/Layout/Home';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import GlobalStyle from './Component/GlobalStyle';
 import Portal from './GuestBook/Layout/Portal';
+import Login from './GuestBook/Layout/Login';
 
 function App() {
   return ( 
@@ -12,6 +13,7 @@ function App() {
       <Routes>
           <Route path="/" element={<Home />}/>
           <Route path="/portal" element={<Portal />}/>
+          <Route path="/login" element={<Login />}/>
       </Routes>
     </BrowserRouter>
   );
