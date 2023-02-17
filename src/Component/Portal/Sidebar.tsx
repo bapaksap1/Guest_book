a66e98e24198d53cafaf6c79254d7f2b4095e7ba
Naufal Fakhri <nfakhri1412@gmail.com>
2023-02-17 19:54:06 +0700

    Update table

--- src/Component/Portal/Sidebar.tsx
+++ src/Component/Portal/Sidebar.tsx
@@ -14,24 +14,22 @@ import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import InboxIcon from '@mui/icons-material/Inbox';
 import MailIcon from '@mui/icons-material/Mail';
-
+import BasicTable from '../Table';
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import PeopleIcon from '@mui/icons-material/People';
+import LogoutIcon from '@mui/icons-material/Logout';
 import Dashboard from './Dashboard';
 import AddGuest from './AddGuest';
+import TableComponent from '../Table';
+import styled from "styled-components"
+import logo from '../../Lambang_Polda_DIY.png' 
 
 const drawerWidth = 240;
 
 const PortalComponent = () => {
-  const [menu, setMenu] = useState("dashboard")
+  const [menu, setMenu] = useState("Daftar Tamu")
   return (
     <Box sx={{ display: 'flex' }}>
-      <CssBaseline />
-      <AppBar position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
-        <Toolbar>
-          <Typography variant="h6" noWrap component="div">
-            Clipped drawer
-          </Typography>
-        </Toolbar>
-      </AppBar>
       <Drawer
         variant="permanent"
         sx={{
@@ -40,14 +38,16 @@ const PortalComponent = () => {
           [`& .MuiDrawer-paper`]: { width: drawerWidth, boxSizing: 'border-box' },
         }}
       >
-        <Toolbar />
+        <Logo src={logo} alt="logo" />
+        <P> Kepolisian Daerah<br/>Daerah Istimewa Yogyakarta</P>
+        <Divider />
         <Box sx={{ overflow: 'auto' }}>
           <List>
-            {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+            {['Dashboard', 'Daftar Tamu'].map((text, index) => (
               <ListItem key={text} button disablePadding onClick={() => setMenu(text)}>
                 <ListItemButton>
                   <ListItemIcon>
-                    {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                    {index % 2 === 0 ? <DashboardIcon /> : <PeopleIcon />}
                   </ListItemIcon>
                   <ListItemText primary={text} />
                 </ListItemButton>
@@ -56,32 +56,52 @@ const PortalComponent = () => {
           </List>
           <Divider />
           <List>
-            {['All mail', 'Trash', 'Spam'].map((text, index) => (
-              <ListItem key={text} disablePadding>
+              <ListItem key={'Logout'} disablePadding>
                 <ListItemButton>
                   <ListItemIcon>
-                    {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                     <LogoutIcon /> 
                   </ListItemIcon>
-                  <ListItemText primary={text} />
+                  <ListItemText primary={"Logout"} />
                 </ListItemButton>
               </ListItem>
-            ))}
           </List>
         </Box>
       </Drawer>
-      <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-        <Toolbar />
+      
+      <Box component="main" sx={{ flexGrow: 1, p: 3, display: "flex", flexDirection: "column", minHeight: "100vh", gap: "20px", backgroundColor: "#EDEDF0"}}>
+      <Title>
+      <p>{menu}</p>
+      </Title>
+      <Divider sx={{color: "black"}}/>
         {
-          menu === 'Inbox' ?
+          menu === 'Dashboard' ?
           <Dashboard />
           :
-          menu === 'Starred' ?
-          <AddGuest />
+          menu === 'Daftar Tamu' ?
+          <TableComponent />
           : null
         }
+     
       </Box>
     </Box>
   );
 }
 
 export default PortalComponent
+
+const Title = styled.div`
+  z-index: 100;
+  > p {
+    margin: 0;
+    font-family: "Poppins";
+    font-size: 20px;
+  }
+`
+const Logo = styled.img`
+  padding: 20px 80px 0px 80px;
+`
+const P = styled.p`
+  margin: 0;
+  text-align: center;
+  margin-bottom: 20px;
+`
